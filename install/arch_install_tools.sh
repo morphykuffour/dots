eab9eb1ef1799781ec71039a6d6b4dca09e14ce4
Morphy Kuffour <morphy.kuffour@uconn.edu>
2021-08-14 12:51:35 -0400

    few changes

--- install/arch_install_tools.sh
+++ install/arch_install_tools.sh
@@ -1,23 +1,8 @@
 #!/bin/sh 
-# Arch and Ubuntu (WSL2) Setup
-# by Morphy Kuffour
-# License: GNU GPLv3
 
 # Install vim-plug for neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-
-# dircolors installation
-
-mkdir /tmp/LS_COLORS && curl -L https://api.github.com/repos/trapd00r/LS_COLORS/tarball/master | tar xzf - --directory=/tmp/LS_COLORS --strip=1
-
-( cd /tmp/LS_COLORS && sh install.sh )
-
-# To enable the colors, add the following line to your shell's start-up script:
-# For Bourne shell (e.g. ~/.bashrc or ~/.zshrc):
-# . "~/.local/share/lscolors.sh"
-
 
 # Make zsh the default shell
 chsh -s /bin/zsh "$name" >/dev/null 2>&1
@@ -40,19 +25,8 @@ newperms "%wheel ALL=(ALL) ALL #LARBS
 fi
 
 
-# Ubuntu Setup
-ubuntu_linux=$(lsb_release -a)
-if [ $ubuntu_linux = 'Ubuntu' ] ; then # ubuntu
-    #install packages from ~/dotfiles/programs/ubuntu_pkglist.txt
-    sudo apt-get install build-enssential
-    sudo apt-get install gdb
-    sudo apt-get install stow
-    sudo apt-get install dircolors
-    sudo apt-get install tldr
-    sudo apt-get install zsh
-    sudo apt-get install xclip
-    sudo apt-get install xsel
-    sudo apt install x11-apps -y    
-    sudo apt-get -y install python3-pip
-fi
+# dircolors installation
+mkdir /tmp/LS_COLORS && curl -L https://api.github.com/repos/trapd00r/LS_COLORS/tarball/master | tar xzf - --directory=/tmp/LS_COLORS --strip=1
+
+( cd /tmp/LS_COLORS && sh install.sh )
 
