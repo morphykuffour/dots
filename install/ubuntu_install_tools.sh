65266617e8d595b3d72bbca1042c1f19d761c287
morpheus <noreply@github.com>
2021-08-14 12:51:35 -0400

    few changes

--- install/ubuntu_install_tools.sh
+++ install/ubuntu_install_tools.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh 
 
 set -e
 set -x
@@ -11,15 +11,17 @@ if ! command -v gh &> /dev/null ; then
     sudo apt-add-repository https://cli.github.com/packages
 fi
 
-sudo apt update
+sudo apt update -y
 
+# {{{ Tools
 sudo apt-get install -y \
     make cmake git \
     gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip \
     make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-    xz-utils tk-dev libffi-dev liblzma-dev git
+    xz-utils tk-dev libffi-dev manpages-dev liblzma-dev git
 
+# {{{ Neovim
 if ! [ -d $HOME/build/neovim ]; then
     git clone https://github.com/neovim/neovim ~/build/neovim
     cd ~/build/neovim/
@@ -28,29 +30,11 @@ if ! [ -d $HOME/build/neovim ]; then
 fi
 
 # {{{ Python tools
-
 if ! [ -d $HOME/.pyenv ]; then
   curl https://pyenv.run | bash
 fi
 
-# I don't really like having the default packages now.
-# directory="$(pyenv root)/plugins/pyenv-default-packages" 
-# if ! [ -d $directory ]; then
-#   git clone https://github.com/jawshooah/pyenv-default-packages.git $directory
-# fi
-
-# Link the default packages to the right location
-# if ! [ -f "$(pyenv root)/default-packages" ]; then
-#   ln -s "$XDG_CONFIG_HOME/pyenv/default-packages" "$(pyenv root)/default-packages"
-# fi
-
-# Update any packages required.
-# pyenv update
-
-# }}}
-
 # {{{ Rust tools
-# Check that rust is installed... otherwise should run this
 if ! [ -x "$(command -v cargo)" ]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
@@ -75,35 +59,24 @@ if ! [ -d ~/build/delta ]; then
 fi
 
 
-# Alacritty
-# TODO: Disable this and switch to kitty, since it seems to play better for me for some reason
-# if ! [ -d ~/build/alacritty/ ];  then
-#   sudo apt install -y \
-#     cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
-
-#   git clone https://github.com/alacritty/alacritty.git ~/build/alacritty
-#   # Not sure about this command...
-#   $(cd ~/build/alacritty/; cargo build --release; cargo install --path alacritty alacritty)
-# fi
-
-# }}}
-
 # {{{ Zsh
-sudo apt install zsh
+sudo apt install zsh -y
 
-# if ! [ -d $HOME/.config/oh-my-zsh/ ]; then
-#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# fi
+# Make zsh the default shell
+chsh -s /bin/zsh "$name" >/dev/null 2>&1
+sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 
-# if ! [ -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]; then
-#   git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-#   ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-# fi
-# }}}
+# {{{ Github
+sudo apt install gh -y
 
-# Github
-sudo apt install gh
+# dircolors installation
 
-if ! command -v kitty &> /dev/null ; then
-    curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-fi
+mkdir /tmp/LS_COLORS && curl -L https://api.github.com/repos/trapd00r/LS_COLORS/tarball/master | tar xzf - --directory=/tmp/LS_COLORS --strip=1
+
+( cd /tmp/LS_COLORS && sh install.sh )
+
+# {{{ Install Applications
+sudo apt-get install -y \
+     build-enssential gdb stow dircolors tldr zsh xclip xsel x11-apps -y    python3-pip
+
+#install packages from ~/dotfiles/programs/ubuntu_pkglist.txt
