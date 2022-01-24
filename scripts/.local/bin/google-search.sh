944027bda55645310c71520a1976b7c38d7d0049
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-01-23 20:00:28 -0500

    dotfile update

---
+++ scripts/.local/bin/google-search.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+URL='https://www.google.com/search?q='
+QUERY=$(echo '' | dmenu -p "Search:" -fn "JetBrains-Mono-Regular-Nerd-Font-Complete-Mono" -b)
+if [ -n "$QUERY" ]; then
+  xdg-open "${URL}${QUERY}" 2> /dev/null
+  exec i3-msg [class="^Brave$"] focus
+fi
+
