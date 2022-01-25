584c93bea69c74a9a9a0c46af172470de5464756
morpheus <noreply@github.com>
2022-01-25 10:09:06 -0500

    dotfile update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -87,6 +87,7 @@
 (column-number-mode)
 (global-set-key [C-mouse-wheel-up-event]  'text-scale-increase)
 (global-set-key  [C-mouse-wheel-down-event] 'text-scale-decrease)
+(setq custom-file (concat user-emacs-directory "/custom.el"))
 
 ;; (global-display-line-numbers-mode t)
 
@@ -386,51 +387,3 @@ If the new path's directories does not exist, create them."
 ;; (define-key polymode-mode-map "\M-ns" 'ess-rmarkdown)
 (with-eval-after-load 'polymode
   (define-key polymode-minor-mode-map (kbd "<f5>") 'ess-rmarkdown))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
- '(exwm-floating-border-color "#504945")
- '(fci-rule-color "#7c6f64")
- '(highlight-tail-colors ((("#363627" "#363627") . 0) (("#323730" "#323730") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#0d1011" "#fabd2f"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#0d1011" "#b8bb26"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#0d1011" "#928374"))
- '(objed-cursor-color "#fb4934")
- '(package-selected-packages
-   '(ess-smart-equals ess poly-wdl poly-R polymode org-transclusion markdown-preview-mode markdown-preview-eww evil-mu4e mu4e-views olivetti zones all-the-icons-ivy-rich centered-window org-dashboard company-irony irony company hl-todo all-the-icons-completion all-the-icons-dired all-the-icons-ivy doom-themes helpful counsel ansi shut-up epl git commander f s cask a which-key vertico use-package undo-fu rainbow-delimiters pdf-tools org-roam-ui org-roam-bibtex markdown-toc ivy-rich gruvbox-theme exec-path-from-shell evil-commentary evil-collection doom-modeline command-log-mode @))
- '(pdf-view-midnight-colors (cons "#ebdbb2" "#282828"))
- '(rustic-ansi-faces
-   ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
- '(vc-annotate-background "#282828")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#b8bb26")
-    (cons 40 "#cebb29")
-    (cons 60 "#e3bc2c")
-    (cons 80 "#fabd2f")
-    (cons 100 "#fba827")
-    (cons 120 "#fc9420")
-    (cons 140 "#fe8019")
-    (cons 160 "#ed611a")
-    (cons 180 "#dc421b")
-    (cons 200 "#cc241d")
-    (cons 220 "#db3024")
-    (cons 240 "#eb3c2c")
-    (cons 260 "#fb4934")
-    (cons 280 "#e05744")
-    (cons 300 "#c66554")
-    (cons 320 "#ac7464")
-    (cons 340 "#7c6f64")
-    (cons 360 "#7c6f64")))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
