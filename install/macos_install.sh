26eb29405eab9559f8a60a64ac29f2fbd36ae9cb
Morphy Kuffour <morp@Morphys-Mac-mini.local>
2022-02-06 19:59:01 -0500

    dotfile update

---
+++ install/macos_install.sh
@@ -0,0 +1,8 @@
+curl https://sh.rustup.rs -sSf | sh
+cargo install exa mcfly starship atuin
+brew install tldr autojump neovim  yabai zstd stow bat
+
+git clone https://github.com/srijanshetty/zsh-pandoc-completion ~/.zsh/zsh-pandoc-completion
+git clone https://github.com/kutsan/zsh-system-clipboard.git    ~/.zsh/zsh-system-clipboard
+git clone https://github.com/zsh-users/zsh-autosuggestions.git  ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.zsh/fast-syntax-highlighting
