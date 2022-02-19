b8fac115022ef20b2d8df8484e12ff87c28c2287
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-02-19 03:17:54 -0500

    dotfile update

---
+++ emacs/.emacs.d/personal.el
@@ -0,0 +1,13 @@
+;; KEYMAPS
+(global-set-key (kbd "C-h f") #'helpful-callable)
+(global-set-key (kbd "C-h v") #'helpful-variable)
+(global-set-key (kbd "C-h k") #'helpful-key)
+(global-set-key (kbd "C-c C-d") #'helpful-at-point)
+(global-set-key (kbd "C-h F") #'helpful-function)
+(global-set-key (kbd "C-h C") #'helpful-command)
+(global-set-key [C-mouse-wheel-up-event]   'text-scale-increase)
+(global-set-key [C-mouse-wheel-down-event] 'text-scale-decrease)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-c e i") (lambda () (interactive) (find-file "~/dotfiles/emacs/.emacs.d/init.el")))
+(global-set-key (kbd "C-c e p") 'package-install)
+(global-set-key (kbd "C-c e o") 'olivetti-mode)
