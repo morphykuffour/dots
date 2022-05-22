78d96304208cbc18b60a2eba7296faddc2ebbef1
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-05-22 07:46:26 -0400

    added new features to xplr and cleaned up init.el for emacs need to merge nano.el into emacs config

--- xplr/.config/xplr/init.lua
+++ xplr/.config/xplr/init.lua
@@ -1,1 +1,20 @@
-xplr.config.general.show_hidden = true
+-- xplr.config.general.show_hidden = true
+
+local home = os.getenv("HOME")
+package.path = home
+.. "/.config/xplr/plugins/?/init.lua;"
+.. home
+.. "/.config/xplr/plugins/?.lua;"
+.. package.path
+
+
+-- Or
+
+require("qrcp").setup{
+  mode = "action",
+  key = "Q",
+  send_options = "-i wlp2s0",
+  receive_options = "-i wlp2s0",
+}
+
+-- Type `:Q` to send or receive files.
