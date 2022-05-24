e5795d0201ad4313d369b855bc12fd6e8fadce8f
morpheus <noreply@github.com>
2022-05-24 06:58:45 -0400

    dotfile update

--- emacs/.emacs.d/personal.el
+++ emacs/.emacs.d/personal.el
@@ -10,6 +10,7 @@
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-set-key (kbd "C-c e i") (lambda () (interactive) (find-file "~/dotfiles/emacs/.emacs.d/init.el")))
 (global-set-key (kbd "C-c e p") 'package-install)
+(global-set-key (kbd "C-c e m") 'mu4e)
 (global-set-key (kbd "C-c e o") 'olivetti-mode)
 (global-set-key (kbd "C-c d i") 'insdate-insert-current-date) ;TODO fixme
 (global-set-key (kbd "<space><space>") 'previous-buffer)
