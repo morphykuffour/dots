f5b311c183123f17751fc59b65d878b9bf53e47c
morpheus <noreply@github.com>
2022-06-29 18:22:54 -0400

    few changes

--- scripts/.local/bin/google-search.sh
+++ scripts/.local/bin/google-search.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-URL='https://www.google.com/search?q='
-QUERY=$(echo '' | dmenu -p "Search:" -fn "JetBrains-Mono-Regular-Nerd-Font-Complete-Mono" -b)
-if [ -n "$QUERY" ]; then
-  xdg-open "${URL}${QUERY}" 2> /dev/null
-  exec i3-msg [class="^Brave$"] focus
-fi
+# URL='https://www.google.com/search?q='
+# QUERY=$(echo '' | dmenu -p "Search:" -fn "JetBrains-Mono-Regular-Nerd-Font-Complete-Mono" -b)
+# if [ -n "$QUERY" ]; then
+#   xdg-open "${URL}${QUERY}" 2> /dev/null
+#   exec i3-msg [class="^Brave$"] focus
+# fi
 
+
+# echo "" | rofi -dmenu -p "Search:" | xargs -I{} xdg-open https://www.google.de/search?q={}
+
+surfraw -browser=$BROWSER $(sr -elvi | awk -F'-' '{print $1}' | sed '/:/d' | awk '{$1=$1};1' | rofi -kb-row-select "Tab" -kb-row-tab "Control+space" -color-window "#000000, #000000, #000000" -color-normal "#000000, #b3e774, #000000, #b3e774, #000000" -color-active "#000000, #b3e774, #000000, #b3e774, #000000" -color-urgent "#000000, #b3e774, #000000, #b3e774, #000000" -dmenu -mesg ">>> Tab = Autocomplete" -i -p "websearch: ")
---
+++ scripts/.local/bin/nixpkg-search.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "" | rofi -dmenu -p "Search:" | xargs -I{} xdg-open "https://search.nixos.org/packages?channel=22.05&from=0&size=50&sort=relevance&type=packages&query="{}
