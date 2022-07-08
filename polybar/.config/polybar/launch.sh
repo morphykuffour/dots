8fcf5f70915f6eece98ef961dad30e5bbf0292d0
morpheus <noreply@github.com>
2022-07-08 16:46:45 -0400

    few chagnes

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,15 +1,15 @@
-#!/bin/env bash
+#!/bin/sh
 
-# Terminate already running bars
+# Terminate already running bar instances
 killall -q polybar
 
-# Wait until bars have been terminated
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar
-# polybar top &
+# Launch bar1 and bar2
+MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
 
-# Manage multiple monitors
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload top &
-done
+MONITORS=$MONITORS polybar top &
+MONITOR=$MONITORS polybar bottom &;
+
+echo "Bars launched..."
