fc7ea4ebc49bed43ffd895c5f7854ef05a37afdd
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-08-01 11:34:15 -0400

    commiting since I haven't in a while

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,11 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("/home/morp/Dropbox/Zettelkasten/client1.org"))
  '(package-selected-packages
-   '(evil-colemak-basics nix-mode lsp-python-ms python-mode doom-modeline helpful sly company-box company eshell-git-prompt vterm dracula-theme nord-theme apropospriate-theme circadian engine-mode which-key vertico use-package undo-fu tree-sitter-langs spacemacs-theme rainbow-delimiters org-roam-ui olivetti moody minions magit-popup ivy-rich hl-todo gruvbox-theme git-timemachine git-commit flycheck exec-path-from-shell evil-surround evil-org evil-commentary evil-collection dired-open dired-hide-dotfiles deadgrep dashboard counsel async all-the-icons)))
+   '(pdf-tools highlight lua-mode org-appear org-mime smex org-babel-eval-in-repl org-superstar evil-colemak-basics nix-mode lsp-python-ms python-mode doom-modeline helpful sly company-box company eshell-git-prompt vterm dracula-theme nord-theme apropospriate-theme circadian engine-mode which-key vertico use-package undo-fu tree-sitter-langs spacemacs-theme rainbow-delimiters org-roam-ui olivetti moody minions magit-popup ivy-rich hl-todo gruvbox-theme git-timemachine git-commit flycheck exec-path-from-shell evil-surround evil-org evil-commentary evil-collection dired-open dired-hide-dotfiles deadgrep dashboard counsel async all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(italic ((t (:slant italic)))))
+ '(italic ((t (:slant italic))))
+ '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button))))))
