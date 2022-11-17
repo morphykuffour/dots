00aca9cfcc56ec9180e141540726159b0d280879
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-17 16:22:21 -0500

    few changes

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,6 +4,7 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (package-initialize)
