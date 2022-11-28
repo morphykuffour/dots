9999f3bb9320a1a3986a2be0cbf9194aa921696e
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-11-27 22:55:04 -0500

    few changes to init.el

--- emacs/.emacs.d/keymaps.el
+++ emacs/.emacs.d/keymaps.el
@@ -1,11 +1,13 @@
 ;; KEYMAPS
 
+;; help
 (global-set-key (kbd "C-h f")   #'helpful-callable)
 (global-set-key (kbd "C-h v")   #'helpful-variable)
 (global-set-key (kbd "C-h k")   #'helpful-key)
 (global-set-key (kbd "C-c C-d") #'helpful-at-point)
 (global-set-key (kbd "C-h F")   #'helpful-function)
 (global-set-key (kbd "C-h C")   #'helpful-command)
+
 ;; (global-set-key [C-mouse-wheel-up-event]   'text-scale-increase)
 ;; (global-set-key [C-mouse-wheel-down-event] 'text-scale-decrease)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -15,6 +17,8 @@
 (global-set-key (kbd "C-c e m") 'mu4e)
 (global-set-key (kbd "C-c e o") 'olivetti-mode)
 (global-set-key (kbd "C-c e r") 'org-babel-execute-src-block)
-(global-set-key (kbd "C-c d i") 'insdate-insert-current-date) ;TODO fixme
-(global-set-key (kbd "<space><space>") 'previous-buffer)
-(global-set-key (kbd "C-x |") 'toggle-window-split)
+(global-set-key (kbd "C-c e b") 'eval-buffer)
+
+;; (global-set-key (kbd "C-c d i") 'insdate-insert-current-date) ;TODO fixme
+;; (global-set-key (kbd "C-x |") 'toggle-window-split);TODO fixme
+;; (global-set-key (kbd "<space><space>") 'previous-buffer);TODO fixme
