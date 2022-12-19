078f54a421c4a759badcc3581d6e99fced819f9a
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-12-19 15:12:17 -0500

    few changes

--- nixpkgs/.config/nixpkgs/config.nix
+++ nixpkgs/.config/nixpkgs/config.nix
@@ -2,3 +2,13 @@
   allowUnfree = true;
   allowUnsupportedSystem = true;
 }
+# https://stackoverflow.com/questions/45417479/how-can-i-use-a-nix-configuration-when-not-using-nixos
+# let
+#     pkgs = import <nixpkgs> {};
+# in
+#     {    
+#         packages = [
+#             pkgs.vim
+#             pkgs.gimp
+#         ];
+#     }
