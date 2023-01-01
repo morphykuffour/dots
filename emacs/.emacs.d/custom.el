10ea60c2897739b6152399dc41d4b8ee43c7648f
morpheus <noreply@github.com>
2023-01-01 14:01:39 -0500

    emacs refactor

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(org-agenda-files nil)
+ '(org-agenda-files '("~/Dropbox/Zettelkasten/agenda/blank.org"))
  '(package-selected-packages
    '(all-the-icons-dired magit org-msg pyenv-mode dwim-shell-command oauth2-request oauth2 org-gcal pdf-tools highlight lua-mode org-appear org-mime smex org-babel-eval-in-repl org-superstar evil-colemak-basics nix-mode lsp-python-ms python-mode doom-modeline helpful sly company-box company eshell-git-prompt vterm dracula-theme nord-theme apropospriate-theme circadian engine-mode which-key vertico use-package undo-fu tree-sitter-langs spacemacs-theme rainbow-delimiters org-roam-ui olivetti moody minions magit-popup ivy-rich hl-todo gruvbox-theme git-timemachine git-commit flycheck exec-path-from-shell evil-surround evil-org evil-commentary evil-collection dired-open dired-hide-dotfiles deadgrep dashboard counsel async all-the-icons))
  '(tool-bar-mode nil))
