a349eb191f7872dd5d678eddcb705e9400bd7389
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-01-02 10:33:11 -0500

    fixing issues in org-mode file

--- emacs/.emacs.d/org-clock-save.el
+++ emacs/.emacs.d/org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-clock-save.el - xps17-nixos at <2023-01-02 Mon 09:49>
+;; org-clock-save.el - xps17-nixos at <2023-01-02 Mon 10:19>
+(setq org-clock-stored-history '(("/home/morp/git/org/refile.org" . 1)))
