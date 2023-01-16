c5f7dea3373c2b0eea609ee56f0a40387a93fefa
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-01-16 17:46:14 -0500

    added ranger plugin and script to install the plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,18 +43,18 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 vim.cmd([[runtime! vimfiles/*.vim]])
 
 -- gx => open url in browser
-if vim.fn.has("wsl") then
-	vim.g.netrw_browsex_viewer = "/usr/bin/wslview"
-	vim.g.clipboard = {
-		name = "wsl clipboard",
-		copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
-		paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
-		cache_enabled = true,
-	}
-elseif vim.fn.has("mac") then
-	vim.g.netrw_browsex_viewer = "open"
-elseif vim.fn.has("linux") then
-	-- vim.g.netrw_browsex_viewer = "/usr/bin/xdg-open"
-	vim.g.netrw_browsex_viewer = "/run/current-system/sw/bin/xdg-open"
-	-- vim.g.netrw_browsex_viewer = vim.env.BROWSER
-end
+-- if vim.fn.has("wsl") then
+-- 	vim.g.netrw_browsex_viewer = "/usr/bin/wslview"
+-- 	vim.g.clipboard = {
+-- 		name = "wsl clipboard",
+-- 		copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+-- 		paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+-- 		cache_enabled = true,
+-- 	}
+-- elseif vim.fn.has("mac") then
+-- 	vim.g.netrw_browsex_viewer = "open"
+-- elseif vim.fn.has("linux") then
+-- 	-- vim.g.netrw_browsex_viewer = "/usr/bin/xdg-open"
+-- 	vim.g.netrw_browsex_viewer = "/run/current-system/sw/bin/xdg-open"
+-- 	-- vim.g.netrw_browsex_viewer = vim.env.BROWSER
+-- end
