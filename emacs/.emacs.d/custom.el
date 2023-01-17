c1318a8a3809af41ad7cb361ba988958de450888
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-01-17 13:04:17 -0500

    few changes

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,17 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-safe-themes
-   '("b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" "3199be8536de4a8300eaf9ce6d864a35aa802088c0925e944e2b74a574c68fd0" "7dc296b80df1b29bfc4062d1a66ee91efb462d6a7a934955e94e786394d80b71" default))
- '(package-selected-packages
-   '(all-the-icons-ivy modus-themes zig-mode zzz-to-char emacsql-sqlite3 sqlite emacsql-pg all org-roam bbdb all-the-icons-dired magit org-msg pyenv-mode dwim-shell-command oauth2-request oauth2 org-gcal pdf-tools highlight lua-mode org-appear org-mime smex org-babel-eval-in-repl org-superstar evil-colemak-basics nix-mode lsp-python-ms python-mode doom-modeline helpful sly company-box company eshell-git-prompt vterm dracula-theme nord-theme apropospriate-theme circadian engine-mode which-key vertico use-package undo-fu tree-sitter-langs spacemacs-theme rainbow-delimiters org-roam-ui olivetti moody minions magit-popup ivy-rich hl-todo gruvbox-theme git-timemachine git-commit flycheck exec-path-from-shell evil-surround evil-org evil-commentary evil-collection dired-open dired-hide-dotfiles deadgrep dashboard counsel async all-the-icons))
- '(tool-bar-mode nil))
+ '(org-agenda-files
+   '("~/iCloud/Org/Todo.org" "/home/morp/Org/agenda/tasks.org" "/home/morp/Org/agenda/school.org" "/home/morp/Org/agenda/birthdays.org" "/home/morp/Org/agenda/habits.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#ffffff" :foreground "#000000" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "JetBrainsMono Nerd Font Mono"))))
- '(italic ((t (:slant italic))))
- '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button))))))
+ '(italic ((t (:slant italic)))))
