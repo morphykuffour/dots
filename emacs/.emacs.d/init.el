88d0ceb40a57b47efda13349145da24a7879582b
morpheus <noreply@github.com>
2023-01-25 12:23:47 -0500

    added s2week2 for org-agenda

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -648,7 +648,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/Org/zettelkasten/20230115133855-s22week0.org" "/home/morp/iCloud/Org/Todo.org" "/home/morp/Org/agenda/tasks.org" "/home/morp/Org/agenda/school.org" "/home/morp/Org/agenda/birthdays.org" "/home/morp/Org/agenda/habits.org")))
+   '("~/Org/zettelkasten/20230125104810-s22week2.org" "/Users/user/Org/zettelkasten/20230115133855-s22week0.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
