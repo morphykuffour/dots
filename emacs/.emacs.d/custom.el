891eede353ee0d5a03a00f1204666a16de96026a
morpheus <noreply@github.com>
2023-01-25 09:41:43 -0500

    few changes

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/iCloud/Org/Todo.org" "/home/morp/Org/agenda/tasks.org" "/home/morp/Org/agenda/school.org" "/home/morp/Org/agenda/birthdays.org" "/home/morp/Org/agenda/habits.org")))
+   '("~/Org/zettelkasten/20230115133855-s22week0.org" "/Users/user/Org/agenda/tasks.org" "/Users/user/Org/agenda/school.org" "/Users/user/Org/agenda/birthdays.org" "/Users/user/Org/agenda/habits.org"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
