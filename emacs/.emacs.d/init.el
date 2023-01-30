5cde4929ec4e33504b3c8d818f1924a79e42a812
morpheus <noreply@github.com>
2023-01-30 13:12:55 -0500

    fixed merge conflict

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -648,11 +648,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-<<<<<<< Updated upstream
    '("~/Org/zettelkasten/20230125104810-s22week2.org" "~/Org/zettelkasten/20230115133855-s22week0.org" "/Users/user/Org/agenda/tasks.org" "/Users/user/Org/agenda/school.org" "/Users/user/Org/agenda/birthdays.org" "/Users/user/Org/agenda/habits.org")))
-=======
-   '("~/Org/zettelkasten/20230130101536-s22week3.org" "/home/morp/iCloud/Org/Todo.org" "/home/morp/Org/agenda/tasks.org" "/home/morp/Org/agenda/school.org" "/home/morp/Org/agenda/birthdays.org" "/home/morp/Org/agenda/habits.org")))
->>>>>>> Stashed changes
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
