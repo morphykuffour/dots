c21550bcdbc3f844acaa93a9f42c87471185d9af
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-04-05 13:19:26 -0400

    few changes

---
+++ nvim/.config/nvim/after/ftplugin/r.lua
@@ -0,0 +1,17 @@
+-- Show line after desired maximum text width
+vim.opt_local.colorcolumn = "81"
+
+-- Keybindings
+vim.api.nvim_buf_set_keymap(0, "i", "<M-i>", " <- ", { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "i", "<M-p>", " |>", { noremap = true })
+
+-- Indentation
+-- Don't align indentation of function args on new line with opening `(`
+vim.g.r_indent_align_args = 0
+
+-- Disable ESS comments
+vim.g.r_indent_ess_comments = 0
+vim.g.r_indent_ess_compatible = 0
+
+-- Use specific comment headers
+vim.opt.comments = [[:#',:###,:##,:#]]
