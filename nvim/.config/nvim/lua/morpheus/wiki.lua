ee8320e4cd26e19b6f7022f57bad76a1e93ae43d
Morphy Kuffour <morphy.kuffour@uconn.edu>
2023-08-28 10:49:36 -0400

    cleaning up dropbox

--- nvim/.config/nvim/lua/morpheus/wiki.lua
+++ nvim/.config/nvim/lua/morpheus/wiki.lua
@@ -1,11 +1,11 @@
 local wiki = {}
 
 wiki.make_diary_entry = function()
-	vim.cmd(string.format(":85vnew ~/Dropbox/notes/note-%s.md", os.date("%y_%m_%d")))
+	vim.cmd(string.format(":85vnew ~/iCloud/notes/note-%s.md", os.date("%y_%m_%d")))
 end
 
 wiki.make_todo = function()
-	local bufnr = vim.fn.bufnr(vim.fn.expand("~/Dropbox/todo/todo.md"), true)
+	local bufnr = vim.fn.bufnr(vim.fn.expand("~/iCloud/todo/todo.md"), true)
 
 	for _, win_id in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
 		local open_bufnr = vim.api.nvim_win_get_buf(win_id)
