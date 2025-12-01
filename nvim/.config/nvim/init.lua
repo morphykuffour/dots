dba610f06500f000a41c5d52e58c97625851a098
morpheus <noreply@github.com>
2025-12-01 12:08:24 -0500

    adding config for clipboard integration over ssh

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -408,4 +408,33 @@ highlight DiffDelete cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Re
 highlight DiffChange cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Red
 ]])
 
+-- Copy over ssh
+if vim.env.SSH_TTY then
+  local osc52 = require("vim.ui.clipboard.osc52")
 
+  local function copy_reg(reg)
+    local orig = osc52.copy(reg)
+    return function(lines, regtype)
+      -- Write to Vim's internal register
+      vim.fn.setreg(reg, table.concat(lines, "\n"), regtype)
+
+      -- Send OSC52 to local clipboard
+      orig(lines, regtype)
+    end
+  end
+
+  vim.g.clipboard = {
+    name = "OSC 52 with register sync",
+    copy = {
+      ["+"] = copy_reg("+"),
+      ["*"] = copy_reg("*"),
+    },
+    -- Do NOT use OSC52 paste, just use internal registers
+    paste = {
+      ["+"] = function() return vim.fn.getreg('+'), 'v' end,
+      ["*"] = function() return vim.fn.getreg('*'), 'v' end,
+    },
+  }
+
+  vim.o.clipboard = "unnamedplus"
+end
